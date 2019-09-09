ba6071a93fabf026eb8343a2b734cd05757ae208
Renata0121 <54933531+Renata0121@users.noreply.github.com>
2019-09-10 00:19:04 +0300

    Add files via upload

---
+++ лаба2.1оп.cpp
@@ -0,0 +1,33 @@
+﻿// лаба2.1оп.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float a, b, c;
+	cout << "Введите числа";
+	cin >>a >> b >> c;
+	float min = a;
+	float max = a;
+	if (b < min) {
+		min = b;
+	}
+	if (c<min) {
+		min = c;
+	}
+	if (b > max) {
+		max = b;
+	}
+	if (c > max) {
+		max = c;
+	}
+	cout << "Max=" << max;
+	cout << "Min=" << min;
+	return 0;
+}
+
+
---
+++ лаба2.4.2.cpp
@@ -0,0 +1,41 @@
+﻿// лаба2.4.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float a, b, c, d, x, x1, x2;
+	cout << "Введите числа";
+	cin >> a >> b >> c;
+	d = b * b - 4 * a * c;
+	x = -c / b;
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
+	if (a == 0) {
+		if (b == 0) {
+			if (c == 0) {
+				cout << "Множество решений";
+			}
+			else {
+				cout << "Решений нет";
+			}
+		}
+		else {
+			cout << "x=" << x;
+		}
+	}
+	else {
+		if (d >= 0) {
+			cout << "x1=" << x1 << endl;
+			cout << "x2=" << x2;
+		}
+		else {
+			cout << "Комплексные корни";
+		}
+	}
+	return 0;
+}
